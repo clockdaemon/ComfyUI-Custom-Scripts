ecbbc277308464e4b809835fd68955b186b55e2c
akatsu.h <akatsusan@gmail.com>
2025-11-29 10:05:27 +0900

    CheckpointLoaderSimpleWithImagesを拡張し、チェックポイント名を出力するクラスを追加

---
+++ py/checkpoint_loader_extended.py
@@ -0,0 +1,26 @@
+from py.better_combos import CheckpointLoaderSimpleWithImages
+
+
+class CheckpointLoaderSimpleWithImagesAndName(CheckpointLoaderSimpleWithImages):
+    """拡張版: CheckpointLoaderSimpleWithImages + ckpt_name出力"""
+    
+    RETURN_TYPES = (*CheckpointLoaderSimpleWithImages.RETURN_TYPES, "STRING",)
+    RETURN_NAMES = (*CheckpointLoaderSimpleWithImages.RETURN_NAMES, "ckpt_name")
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return super().INPUT_TYPES()
+
+    def load_checkpoint(self, **kwargs):
+        ckpt_name = kwargs.get("ckpt_name", "")
+        result = super().load_checkpoint(**kwargs)
+        return (*result, ckpt_name)
+
+
+NODE_CLASS_MAPPINGS = {
+    "CheckpointLoader|pysssss|extended": CheckpointLoaderSimpleWithImagesAndName,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "CheckpointLoader|pysssss|extended": "Checkpoint Loader Extended 🐍",
+}
